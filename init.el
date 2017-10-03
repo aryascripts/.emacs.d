a865cb0b725eb7a775e6f786d55088bcd01d3ef2
Aman Bhimani <amanb014@gmail.com>
2017-10-03 14:28:14 -0400

    added evil mode

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
     (tooltip-mode -1))
 
 (setq inhibit-startup-message t)
-(setq initial-scratch-message "")
+; (setq initial-scratch-message "")
 
 ;; Set up package
 (require 'package)
@@ -30,3 +30,28 @@
 
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
+
+;;; This is for vim / evil mode in emacs.
+;;; from purcell/emacs.d
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(package-initialize)
+
+(require-package 'evil)
+
+(setq evil-search-module 'evil-search
+      evil-want-C-u-scroll t
+      evil-want-C-w-in-emacs-state t)
+
+(require 'evil)
+(evil-mode t)
